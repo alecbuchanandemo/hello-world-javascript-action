b051041c497f0ff568f32024aa47ed5647679b44
Alec Buchanan <alecbuchanan@github.com>
2023-12-13 21:38:45 +0000

    Update action.yml and index.js

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const { execSync } = require('child_process');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function main() {
+  try {
+    const dstatOutput = execSync('dstat --nocolor --noupdate --output dstat.csv 1 1', { encoding: 'utf8' });
+    const lines = dstatOutput.split('\n');
+    const stats = lines[lines.length - 2].split(',');
+
+    const cpu = stats[3];
+    const ram = stats[6];
+    const io = stats[8];
+
+    await core.summary
+      .addHeading('Resource Usage')
+      .addTable([
+        [{data: 'Resource', header: true}, {data: 'Usage', header: true}],
+        ['CPU', `${cpu}%`],
+        ['RAM', `${ram}%`],
+        ['I/O', `${io}%`]
+      ])
+      .write();
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+main();
